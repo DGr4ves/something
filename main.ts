410036eff55d9920180a43ebc1b14f9d925ce241
DGr4ves <102975825+DGr4ves@users.noreply.github.com>
2022-04-05 12:57:48 -0600

    When Button A pressed, arrow points East because why the heck not

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showArrow(ArrowNames.East)
+})
 basic.showString("Hello There")
 basic.forever(function () {
     basic.showArrow(ArrowNames.North)
